d6aaa157ef1b3b984e39a2e79fe7a28592d68b5a
Denis Maltsev <demaltsev@gmail.com>
2021-08-02 11:54:06 +0300

    Dependency fix

--- Package.swift
+++ Package.swift
@@ -12,17 +12,14 @@ let package = Package(
             targets: ["webim-client-library"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "webim-client-library",
-            dependencies: [
-                .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0")
-            ]),
+            dependencies: []),
         .testTarget(
             name: "webim-client-libraryTests",
             dependencies: ["webim-client-library"]),
