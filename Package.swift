dd749f656cb12087bfabf1df6516ea9d7243543b
Denis Maltsev <demaltsev@gmail.com>
2021-08-02 12:02:26 +0300

    Dependency fixed

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "webim-client-library",
-            dependencies: ["SQLite.swift"]),
+            dependencies: ["SQLite"]),
         .testTarget(
             name: "webim-client-libraryTests",
             dependencies: ["webim-client-library"]),
