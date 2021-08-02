c18ebca03584a4d462396ae70d4d283a156b6aac
Denis Maltsev <demaltsev@gmail.com>
2021-08-02 12:07:38 +0300

    Test #1

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "webim-client-library",
-            dependencies: ["SQLite.swift"]),
+            dependencies: [
+                .product(name: "SQLite", package: "SQLite.swift")
+            ]),
         .testTarget(
             name: "webim-client-libraryTests",
             dependencies: ["webim-client-library"]),
